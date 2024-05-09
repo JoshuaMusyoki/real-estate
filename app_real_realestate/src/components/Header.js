954de690472bbb7de25e409e9d8e648306981c9e
Njeri-Kamau <njeerikamau2@gmail.com>
2024-05-09 14:45:34 +0300

    updates to header file

--- app_real_realestate/src/components/Header.js
+++ app_real_realestate/src/components/Header.js
@@ -1,10 +1,10 @@
 
-const Header =() =>{
+const Header = () =>{
     return(
-        <header style={{position: 'fixed', top: 0, left: 0, width: '100%', padding: '1rem', backgroundColor: '#f0f0f0' }}>
-            <h1>Donation Platform</h1>
-            <nav>
-                <a href="/">Home</a>
+        <header style={{position: 'fixed', top: 0, left: 0, width: '100%', padding: '1rem', backgroundColor: '#f0f0f0', display: 'flex', gap: '60%' }}>
+            <h1 style={{width: '200px'}} >Donation Platform</h1>
+            <nav style={{display: 'flex', justifyContent: 'space-around', width: '500px'}}>
+                <a style={{}} href="/">Connect Wallet</a>
                 <a href="/donors">Donors</a>
                 <a href="/victims">Victims</a>
                 <a href="/property-owners">Property Owners</a> 
@@ -13,3 +13,5 @@ const Header =() =>{
     );
 };
 export default Header;
+
+
