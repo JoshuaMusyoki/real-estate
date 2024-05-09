90d409a0bc3afccdc208966d0161ca900aaf888a
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-05-09 16:11:05 +0300

    Modifications on LandingPage

--- app_real_realestate/src/components/Header.js
+++ app_real_realestate/src/components/Header.js
@@ -1,7 +1,7 @@
 
 const Header =() =>{
     return(
-        <header style={{position: 'fixed', top: 0, left: 0, width: '100%', padding: '1rem', backgroundColor: '#f0f0f0' }}>
+        <header style={{position: 'fixed', top: 0,justifyContent:"center", width: '100%', padding: '5 rem', backgroundColor: '#f0f0f0' }}>
             <h1>Donation Platform</h1>
             <nav>
                 <a href="/">Home</a>
--- app_real_realestate/src/pages/LandingPage.js
+++ app_real_realestate/src/pages/LandingPage.js
@@ -1,3 +1,4 @@
+import { Button } from 'bootstrap';
 import React from 'react'
 
 const LandingPage =()=>{
@@ -5,13 +6,24 @@ const LandingPage =()=>{
         console.log(`User selected: ${userType}`);
     };
     return (
-        <div className="landing-page">
+        <div className="landing-page" 
+        style={{display:'flex', flexDirection:'column', alignItems:'center', marginTop:"10%"}}>
             <h1>Donation Platform</h1>
             <p>Welcome! Let us know how you can contribute.</p>
-        <div className="button-container">
-            <button onClick={() => handleButtonClick('Donor')}>Donor</button> <br/>
-            <button onClick={() => handleButtonClick('Victim')}>Victim</button><br/>
-            <button onClick={() => handleButtonClick('Property Owner')}>Property Owner</button>
+        <div className="button-container"
+        style={{padding:'2%', alignItems:'center'}}
+        >
+            <button 
+            style={{border: 'solid 1px gray', color: 'white',padding: '8px', backgroundColor: 'green', borderRadius: '8px'}}
+            onClick={() => handleButtonClick('Donor')}>Donor</button> 
+            <br/>
+
+            <button
+            style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'maroon', borderRadius: '8px'}}
+             onClick={() => handleButtonClick('Victim')}>Victim</button><br/>
+            <button 
+            style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'maroon', borderRadius: '8px'}}
+            onClick={() => handleButtonClick('Property Owner')}>Property Owner</button>
         </div>
        </div>
     );
