2f7ec624588d498ab890f46e25f43c9a30a86105
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-05-09 17:11:18 +0300

    changes

--- app_real_realestate/src/components/Header.js
+++ app_real_realestate/src/components/Header.js
@@ -1,11 +1,10 @@
-import logo from "../iconz/logo.png"
-import wallet from "../iconz/wallet.png"
-const Header = () =>{
+
+const Header =() =>{
     return(
-        <header style={{position: 'fixed', top: 0, left: 0, width: '100%', padding: '1rem', backgroundColor: '#f0f0f8', display: 'flex', gap: '60%' }}>
-            <h1 style={{width: '400px'}} ><img style={{float: 'left', marginRight: '10px'}} src={logo} alt="logo" />Donation Platform</h1>
-            <nav style={{display: 'flex', justifyContent: 'space-around', width: '800px', padding: '5px'}}>
-                <button><img style={{float: 'right', marginLeft: '10px'}} src={wallet} alt="logo" />Connect</button>
+        <header style={{position: 'fixed', top: 0, left: 0, width: '100%', padding: '1rem', backgroundColor: '#f0f0f0' }}>
+            <h1>Donation Platform</h1>
+            <nav>
+                <a href="/">Home</a>
                 <a href="/donors">Donors</a>
                 <a href="/victims">Victims</a>
                 <a href="/property-owners">Property Owners</a> 
--- app_real_realestate/src/pages/PropertyOwners.js
+++ app_real_realestate/src/pages/PropertyOwners.js
@@ -3,7 +3,7 @@ import React from 'react'
 function PropertyOwners() {
   return (
     <>
-        <form style={{display: 'flex', flexDirection: 'column', marginTop: '12%', marginLeft: '15%', backgroundColor: '#f0f0f8', width: '70%', borderRadius: '8px', padding: '3%'}} >
+        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10%', marginLeft: '15%', backgroundColor: '#f0f0f8', width: '70%', borderRadius: '8px'}} >
         <p style={{fontSize: '32px', fontWeight: 'bold'}}>Property Owners</p>
         <label>Name</label>
         <input style={{border: 'solid 1px'}}/>
@@ -13,10 +13,10 @@ function PropertyOwners() {
         <input style={{border: 'solid 1px'}}/>
         <label>Is Available?</label>
         <div style={{display: 'flex', gap: '20px'}}>
-        <button style={{border: 'solid 1px gray', color: 'white', width: '100px', padding: '8px', backgroundColor: 'green', borderRadius: '8px'}} >Yes</button> <br />
-        <button style={{border: 'solid 1px gray', color: 'white', width: '100px', padding: '8px', backgroundColor: 'maroon', borderRadius: '8px'}} >No</button>
+        <button style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'green', borderRadius: '8px'}} >Yes</button> <br />
+        <button style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'maroon', borderRadius: '8px'}} >No</button>
         </div> <br />
-        <button type='submit' style={{border:'solid 1px gray', color: 'white', width: '50%', alignSelf: 'center', backgroundColor: 'green', padding: '8px', borderRadius: '8px' }}>Withdraw</button>
+        <button type='submit' style={{border:'solid 1px gray', color: 'white', backgroundColor: 'green', padding: '8px', borderRadius: '8px' }}>Withdraw</button>
       </form>
     </>
   )
