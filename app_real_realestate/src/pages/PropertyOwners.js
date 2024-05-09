c0d0d52fd239b558cd5f27be72e2a174ee7cc920
Njeri-Kamau <njeerikamau2@gmail.com>
2024-05-09 15:12:08 +0300

    Donor and PropertyOwner updates

--- app_real_realestate/src/pages/PropertyOwners.js
+++ app_real_realestate/src/pages/PropertyOwners.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function PropertyOwners() {
   return (
-    <div>PropertyOwners</div>
+    <>
+        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10%', marginLeft: '15%', backgroundColor: '#f0f0f8', width: '70%', borderRadius: '8px'}} >
+        <p style={{fontSize: '32px', fontWeight: 'bold'}}>Property Owners</p>
+        <label>Name</label>
+        <input style={{border: 'solid 1px'}}/>
+        <label>Available Units</label>
+        <input style={{border: 'solid 1px'}}/>
+        <label>Price per Unit</label>
+        <input style={{border: 'solid 1px'}}/>
+        <label>Is Available?</label>
+        <div style={{display: 'flex', gap: '20px'}}>
+        <button style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'green', borderRadius: '8px'}} >Yes</button> <br />
+        <button style={{border: 'solid 1px gray', color: 'white', padding: '8px', backgroundColor: 'maroon', borderRadius: '8px'}} >No</button>
+        </div> <br />
+        <button type='submit' style={{border:'solid 1px gray', color: 'white', backgroundColor: 'green', padding: '8px', borderRadius: '8px' }}>Submit</button>
+      </form>
+    </>
   )
 }
 
